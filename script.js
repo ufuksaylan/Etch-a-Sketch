2c35579d5ef9ac698420f6e00deae63b4d1b04f9
ufuksaylan <ufuksaylannn@gmail.com>
2022-02-26 19:10:15 +0200

    fix the bug of for loop

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const sketch = document.querySelector('#sketch');
 
-let totalDiv = 36;
-let divWidth = (400/Math.sqrt(totalDiv))-2;
+let totalDiv = 16;
+let divWidth = (400/totalDiv) -2;
 
-for (let i = 0; i < totalDiv; i++) {
+for (let i = 0; i < totalDiv *totalDiv; i++) {
     const newDiv = document.createElement('div'); 
     newDiv.classList.add('divs');
     newDiv.setAttribute('style', `width: ${divWidth}px`);
