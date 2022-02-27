5c303c140b8c4907ab0ea63cde4c0e4dc66ef2e9
ufuksaylan <ufuksaylannn@gmail.com>
2022-02-27 11:57:44 +0200

    added slider bar

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const sketch = document.querySelector('#sketch');
 
-let totalDiv = 16;
-let divWidth = (400/totalDiv) -2;
-
-for (let i = 0; i < totalDiv *totalDiv; i++) {
+function createDivs(totalDiv){
+  let divWidth = (400/totalDiv) -2;
+  for (let i = 0; i < totalDiv *totalDiv; i++) {
     const newDiv = document.createElement('div'); 
     newDiv.classList.add('divs');
     newDiv.setAttribute('style', `width: ${divWidth}px`);
     sketch.appendChild(newDiv);
+  }
 }
+
+createDivs(16);
+
+
+const slider = document.getElementById("DimensionSlider");
+const DimensionValue = document.getElementById("DimensionValue");
+
+
+// Update the current slider value  and sketch (each time you drag the slider handle)
+slider.oninput = function(){
+  // delete divs 
+  document.querySelectorAll('.divs').forEach(e => e.remove());
+
+  DimensionValue.innerHTML = `${slider.value}x${slider.value}`;
+  const totalDiv = slider.value;
+
+  createDivs(totalDiv);
+
+}
+
