8ba57362a350d2f2c08df64ea8981a8d8ef0bea3
ufuksaylan <ufuksaylannn@gmail.com>
2022-02-27 17:07:08 +0200

    add operational buttons to control bar

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 const sketch = document.querySelector('#sketch');
 
-function createDivs(totalDiv){
-  let divWidth = (400/totalDiv) -2;
-  for (let i = 0; i < totalDiv *totalDiv; i++) {
-    const newDiv = document.createElement('div'); 
-    newDiv.classList.add('divs');
-    newDiv.setAttribute('style', `width: ${divWidth}px`);
-    sketch.appendChild(newDiv);
-  }
-}
-
 createDivs(16);
-
 
 const slider = document.getElementById("DimensionSlider");
 const DimensionValue = document.getElementById("DimensionValue");
-
 
 // Update the current slider value  and sketch (each time you drag the slider handle)
 slider.oninput = function(){
@@ -26,6 +14,62 @@ slider.oninput = function(){
   const totalDiv = slider.value;
 
   createDivs(totalDiv);
-
 }
 
+blackMode();
+eraser();
+clean();
+rainbowMode();
+
+function blackMode(){
+  const black = document.querySelector('#blackMod');
+  black.addEventListener('click', () => {
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach(function(e){
+      e.addEventListener('mouseover',  function(){
+        this.style.backgroundColor = 'black';
+      });
+    });
+  });
+}
+function eraser(){
+  const eraser = document.querySelector('#eraser');
+  eraser.addEventListener('click', () => {
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach(function(e){
+      e.addEventListener('mouseover',  function(){
+        this.style.backgroundColor = '#d3d3d3';
+      });
+    });
+  });
+}
+function clean(){
+  const clean = document.querySelector('#clean');
+  clean.addEventListener('click', () => {
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach(function(e){
+      e.style.backgroundColor = '#d3d3d3';
+    });
+  });
+}
+function rainbowMode(){
+  const rainbow = document.querySelector('#rainbowMode');
+  rainbow.addEventListener('click', () => {
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach(function(e){
+      e.addEventListener('mouseover',  function(){
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = `#${randomColor}`;
+      });
+    });
+  });
+}
+function createDivs(totalDiv){
+  let divWidth = (400/totalDiv) -2;
+  for (let i = 0; i < totalDiv *totalDiv; i++) {
+    const newDiv = document.createElement('div'); 
+    newDiv.classList.add('divs');
+    newDiv.setAttribute('style', `width: ${divWidth}px`);
+    sketch.appendChild(newDiv);
+  }
+}
